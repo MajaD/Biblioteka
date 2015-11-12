16a7ee62c11c3e2e879fa8eb6bbb5e1e79e73365
unknown <Dark-Angel93@wp.pl>
2015-11-12 22:16:31 +0100

    model ksiazki

---
+++ src/main/java/model/Book.java
@@ -0,0 +1,106 @@
+
+package model;
+
+/**
+ *
+ * @author Maja
+ */
+public class Book {
+    
+    private int id;
+    private String title;
+    private String category;
+    private String publishingHouse;
+    private String description;
+    private int rate;
+    private String dateOfPublishing;
+    private String dateOfHire;
+    private boolean rent;
+
+    public Book(int id, String title, String category, String publishingHouse, String description, int rate, String dateOfPublishing, String dateOfHire) {
+        this.id = id;
+        this.title = title;
+        this.category = category;
+        this.publishingHouse = publishingHouse;
+        this.description = description;
+        this.rate = rate;
+        this.dateOfPublishing = dateOfPublishing;
+        this.dateOfHire = dateOfHire;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getPublishingHouse() {
+        return publishingHouse;
+    }
+
+    public void setPublishingHouse(String publishingHouse) {
+        this.publishingHouse = publishingHouse;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getRate() {
+        return rate;
+    }
+
+    public void setRate(int rate) {
+        this.rate = rate;
+    }
+
+    public String getDateOfPublishing() {
+        return dateOfPublishing;
+    }
+
+    public void setDateOfPublishing(String dateOfPublishing) {
+        this.dateOfPublishing = dateOfPublishing;
+    }
+
+    public String getDateOfHire() {
+        return dateOfHire;
+    }
+
+    public void setDateOfHire(String dateOfHire) {
+        this.dateOfHire = dateOfHire;
+    }
+
+    public boolean isRent() {
+        return rent;
+    }
+
+    public void setRent(boolean rent) {
+        this.rent = rent;
+    }
+    
+    
+    
+    
+}
